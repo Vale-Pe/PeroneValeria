493fbad2f0a3be0305f4214c1a6c3095abf533a1
Vale-Pe <valeperone@gmail.com>
2022-09-27 23:37:18 -0300

    Desafio 6

---
+++ Desafio6/public/index.js
@@ -0,0 +1,60 @@
+const socket = io.connect(); // Ya podemos empezar a usar los sockets desde el cliente 
+
+//funcion que recibe los productos
+socket.on("products", (data) => { renderProduct(data); });
+
+//funcion que recibe los mensajes
+socket.on('messages', (data) => { renderMessage(data); });
+
+function renderProduct(data) {
+	const html = data
+		.map((producto) => {
+            let str = `<tr class="table-light">
+                        <td>${producto.nombre}</td>
+                        <td>${producto.precio}</td>
+                        <td><img width=50 src='${producto.foto}' alt="imgProducto"></td>
+                        </tr>`
+			return str
+    }).join("\n")
+	document.getElementById("idTbody").innerHTML = html
+}
+
+function addProduct() {
+	const producto = {
+		nombre: document.getElementById("nombre").value,
+		precio: document.getElementById("precio").value,
+        foto: document.getElementById("foto").value
+	}
+
+	socket.emit("new-product", producto)
+}
+
+//funcion para agregar mensaje
+function renderMessage(data) {
+    const html = data.map((elem, index) => {
+        return(`<div class="mensajeChat">
+            <strong class="autorChat">${elem.author}</strong>
+            <p class="fechaChat">(${elem.date}):</p>
+            <p class="textoChat">${elem.text}</p> </div>`)
+    }).join(" ");
+    document.getElementById('messages').innerHTML = html;
+
+}
+
+//funcion para enviar mensaje
+function addMessage(e) {
+    const date = new Date()
+    const formatDate = date.toLocaleString();
+
+    const mensaje = {
+        author: document.getElementById('username').value,
+        date: formatDate,
+        text: document.getElementById('texto').value
+    };
+    socket.emit('new-message', mensaje);
+    return false;
+}
+
+function table() {
+
+}
---
+++ Desafio6/server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const PORT = process.env.port || 8080
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+
+// Indicamos que queremos cargar los archivos estáticos que se encuentran en dicha carpeta
+app.use(express.static('./public'))
+
+// Esta ruta carga nuestro archivo index.html en la raíz de la misma
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname })
+})
+
+//variable donde voy a guardar los mensajes
+
+const date = new Date()
+const formatDate = date.toLocaleString();
+
+const messages = [
+    { author: "Juan", date: formatDate, text: "¡Hola! ¿Que tal?" },
+    { author: "Pedro", date: formatDate, text: "¡Muy bien! ¿Y vos?" },
+    { author: "Ana", date: formatDate, text: "¡Genial!" }
+];
+
+const products = [];
+
+io.on('connection', (socket) => { // "connection" se ejecuta la primera vez que se abre una nueva conexión
+    console.log('Nuevo cliente conectado') // Se imprimirá solo la primera vez que se ha abierto la conexión    
+
+    socket.emit('products', products);
+	
+	socket.on('new-product', data => {
+        products.push(data);
+        io.sockets.emit('products', products); //envia todos los mensajes a todos los usuarios
+    });
+
+	//cuando alguien se conecta le llegan todos los mensjaes
+    socket.emit('messages', messages);
+
+//evento que va a estar escuchando cuando manden un mensaje
+    socket.on('new-message', data => {
+        messages.push(data);
+        io.sockets.emit('messages', messages); //envia todos los mensajes a todos los usuarios
+    });
+})
+
+
+// El servidor funcionando en el puerto 8080
+httpServer.listen(PORT, () => console.log('Servidor corriendo en http://localhost:8080'))
